ffa0fa4e01e1e03831eb2ccfa6834d51462501fa
Juan Tascon <juantascon@horlux.org>
2016-01-28 13:58:02 -0500

    using setuptools, added dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 #! /usr/bin/env python3
 
-import sys
 from setuptools import setup
 
 setup(
     name="tgio",
-    description="io shell commands over telegram (echo, read, etc)",
     version="0.0.1",
+    description="io shell commands over telegram (echo, read, etc)",
+    url="https://github.com/juantascon/tgio",
     author="Juan Diego Tascon",
     author_email="juantascon.tgio@horlux.org",
-    
-    url="https://github.com/juantascon/tgio",
     license="GPL3",
-    keywords=["telegram", "io", "shell", "tools", "utils", "echo", "read"],
+    keywords=["telegram", "io", "shell", "tools", "utils", "echo", "read", "printf"],
+    
+    install_requires=[
+        'pyxdg',
+        'pytelegrambotapi'
+    ],
+    
     scripts=["tgio"],
     data_files=[("share/tgio", ["README.md", "LICENSE", "tgio.cfg.example"])]
 )
